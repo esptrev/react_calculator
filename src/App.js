5fcd00b3ea9de987f9925e12540b1923f512e229
Trevor Esparza <trevoresparza30@gmail.com>
2022-08-03 16:49:40 -0500

    layed out grid

---
+++ src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+
+function App() {
+  return (
+      <section className='grid'>
+        <div className='output'>
+          <div className='previous-operand'></div>
+          <div className='current-operand'></div>
+        </div>
+          <button className='spanTwo'></button>
+          <button>DEL</button>
+          <button>/</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>*</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>+</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>-</button>
+          <button>.</button>
+          <button>0</button>
+          <button className='spanTwo'>=</button>
+      </section>
+  );
+}
+
+export default App;
