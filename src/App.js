8f993f439e83564dfc679fb1b8dfae16b5e9f87a
Trevor Esparza <trevoresparza30@gmail.com>
2022-08-04 15:04:12 -0500

    number formatting

--- src/App.js
+++ src/App.js
@@ -118,14 +118,24 @@ function evaluate({currentOperand,previousOperand,operation}){
     return computation.toString()
 }
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+    maximumFractionDigits: 0,
+})
+function formatOperand(operand) {
+    if (operand == null) return
+    const [integer, decimal] = operand.split(".")
+    if (decimal == null) return INTEGER_FORMATTER.format(integer)
+    return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+}
+
 function App() {
 
     const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {});
     return (
         <section className='grid'>
             <div className='output'>
-                <div className='previous-operand'>{previousOperand} {operation}</div>
-                <div className='current-operand'>{currentOperand}</div>
+                <div className='previous-operand'>{formatOperand(previousOperand)} {operation}</div>
+                <div className='current-operand'>{formatOperand(currentOperand)}</div>
             </div>
             <button className='spanTwo'
                     onClick={()=> dispatch({ type: ACTIONS.CLEAR})}>AC</button>
